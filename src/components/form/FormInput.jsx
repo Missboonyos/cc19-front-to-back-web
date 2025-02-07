ae9d1e204c560fc46a8514eaa00cf1f876eb26f3
Misboonyos <boonyos.k@gmail.com>
2025-02-07 11:49:36 +0700

    check errors

--- src/components/form/FormInput.jsx
+++ src/components/form/FormInput.jsx
@@ -1,8 +1,11 @@
 //rfce
 import React from 'react'
 
-function FormInput( {register, name } ) {
+function FormInput( {register, name, errors } ) {
+    console.log(errors[name].message);
+    //  || first true  && first false
   return (
+    <div>    
     <input
         placeholder={name}
         type="text"
@@ -10,6 +13,11 @@ function FormInput( {register, name } ) {
         className='border w-full border-gray-300 
         rounded-md p-1 px-4'               
         /> 
+        {
+            errors[name] && 
+            <p className='text-sm text-red-950'>{errors[name].message}</p>
+        }
+        </div>
   );
 }
 
